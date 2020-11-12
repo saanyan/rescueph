664c770ab274efffa5a9d24f5d22ea6bd0f3012e
saanyan <62740358+saanyan@users.noreply.github.com>
2020-11-13 02:47:28 +0800

    Update insert_function.sql

--- sql/insert_function.sql
+++ sql/insert_function.sql
@@ -1,27 +1,30 @@
-DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text);
+DROP FUNCTION IF EXISTS insert_crowd_mapping_data(text,text,text,text,text,text,text);
 --Assumes only one value being inserted
 
 CREATE OR REPLACE FUNCTION insert_crowd_mapping_data (
     _geojson TEXT,
     _name TEXT,
-    _note TEXT,	
-    _hear TEXT)    
+    _details TEXT,
+    _contact_details TEXT,
+    _address TEXT,
+    _rescued TEXT,	
+    _rescuer_org TEXT)    
 --Has to return something in order to be used in a "SELECT" statement
 RETURNS integer
 AS $$
 DECLARE 
     _the_geom GEOMETRY;
 	--The name of your table in cartoDB
-	_the_table TEXT := 'hearhere';
+	_the_table TEXT := 'rescue';
 BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
 	
 
 	--Executes the insert given the supplied geometry, description, and username, while protecting against SQL injection.
-    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, note, hear)
-            VALUES ($1, $2, $3, $4)
-            ' USING _the_geom, _name, _note, _hear;
+    EXECUTE ' INSERT INTO '||quote_ident(_the_table)||' (the_geom, name, details, contact_details, address, rescued, rescuer_org)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            ' USING _the_geom, _name, _details, _contact_details, _address, _rescued, _rescuer_org;
             
     RETURN 1;
 END;
@@ -29,4 +32,4 @@ $$
 LANGUAGE plpgsql SECURITY DEFINER ;
 
 --Grant access to the public user
-GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data(text, text, text, text) TO publicuser;
+GRANT EXECUTE ON FUNCTION insert_crowd_mapping_data(text, text, text, text, text, text, text) TO publicuser;
